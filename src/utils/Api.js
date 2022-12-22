d8437db3ac7770110128354e909af046c9bb7035
Nguyen Louis <nguyenlouis172@gmail.com>
2022-12-22 16:32:47 +0100

    add filter form

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,6 +1,6 @@
 
 export const getCharacterFromApi = async () => {
-  const req = await fetch('https://rickandmortyapi.com/api/character')
+  const req = await fetch('https://rickandmortyapi.com/api/character/')
   const res = await req.json()
 
   return res.results
