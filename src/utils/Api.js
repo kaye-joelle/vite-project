8990f64e1a34b32b432c48462fd76a3783c65df5
Nguyen Louis <nguyenlouis172@gmail.com>
2022-12-22 17:30:22 +0100

    add filter by genre

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,10 +1,44 @@
 
+//Filter by character
 export const getCharacterFromApi = async () => {
-    const req = await fetch('https://rickandmortyapi.com/api/character/')
+  const req = await fetch('https://rickandmortyapi.com/api/character/')
   const res = await req.json()
 
   return res.results
 }
+
+// //Filter by character gender male
+// export const getCharacterGenderFromApi = async ()=> {
+//   const req = await fetch('https://rickandmortyapi.com/api/character/?gender=male')
+//   const res = await req.json()
+
+//   return res.results
+// }
+
+// //Filter by character gender female 
+// export const getCharacterGenderFromApi = async ()=> {
+//   const req = await fetch('https://rickandmortyapi.com/api/character/?gender=female')
+//   const res = await req.json()
+
+//   return res.results
+// }
+
+// //Filter by character gender genderless
+// export const getCharacterGenderFromApi = async ()=> {
+//   const req = await fetch('https://rickandmortyapi.com/api/character/?gender=genderless')
+//   const res = await req.json()
+
+//   return res.results
+// }
+
+// //Filter by character gender unknown
+// export const getCharacterGenderFromApi = async ()=> {
+//   const req = await fetch('https://rickandmortyapi.com/api/character/?gender=unknown')
+//   const res = await req.json()
+
+//   return res.results
+// }
+
 
 export const getPostFromApi = async () => {
   const req = await fetch('https://jsonplaceholder.typicode.com/posts')
