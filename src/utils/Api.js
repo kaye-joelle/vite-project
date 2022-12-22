120b26896ab9e58054c748e0eedb1e058c092052
Nguyen Louis <nguyenlouis172@gmail.com>
2022-12-22 15:49:55 +0100

    finish list all characters

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -3,7 +3,7 @@ export const getCharacterFromApi = async () => {
   const req = await fetch('https://rickandmortyapi.com/api/character')
   const res = await req.json()
 
-  return res.data
+  return res.results
 }
 
 export const getPostFromApi = async () => {
