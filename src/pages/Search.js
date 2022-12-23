ce62484cbb3183ac4c5a80a1bc4d5650cbc5e04b
Your Name <jojo-otro@hotmail.fr>
2022-12-23 15:43:31 +0100

    MAJ du filtre Search

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,23 +1,21 @@
 import ListCard from "../components/ListCard"
 import { getCharacterFromApi } from "../utils/Api"
 
-const ListOfCharacter = async (searchCharacter, Param) => {
-  // console.log(searchCharacter);
-
+const ListOfCharacter = async (searchCharacter) => {
   if (searchCharacter){
     searchCharacter = `?name=${searchCharacter}`
   }
   const res = await getCharacterFromApi(searchCharacter)
-  if (res){
+  if (res) {
     const data = res.map((element) => ({
       text: element.name,
       src: element.image   
-  }))
-  return ListOfCharacter(data)
+    }))
+    return ListCard(data)
+  }
+  const element = document.createElement('div');
+  element.textContent= 'Aucun résultat'
+  return element
 }
-const element = document.createElement('div');
-element.textContent= 'Aucun résultat'
-
-return element}
 
 export default ListOfCharacter
